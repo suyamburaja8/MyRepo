d6ad1fe450018467a805d2ba8a3d2a5d43fa43d4
Suyamburaja <suyamburaja@expsoltechs.com>
2020-10-27 15:21:34 +0530

    Design added

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Auth.index');
+});
+
+Route::get('/login',function () {
+    return view('Auth.login');
+});
+
+Route::get('/register',function () {
+    return view('Auth.register');
+});
+
+Route::get('/password',function () {
+    return view('Auth.password');
 });
